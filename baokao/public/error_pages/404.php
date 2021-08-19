e63e5036cb7da8f5e2d34265edd57d53f70380ee
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2021-08-19 13:20:37 +1000

    Add error pages

--- baokao/public/error_pages/404.php
+++ baokao/public/error_pages/404.php
@@ -5,6 +5,7 @@
 <div class="content">
 <div class="flash mt-3 flash-error" style="margin: 20px;" id="no-file" style=""><svg class="octicon octicon-container" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill-rule="evenodd" d="M1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12zm8.036-4.024a.75.75 0 00-1.06 1.06L10.939 12l-2.963 2.963a.75.75 0 101.06 1.06L12 13.06l2.963 2.964a.75.75 0 001.061-1.06L13.061 12l2.963-2.964a.75.75 0 10-1.06-1.06L12 10.939 9.036 7.976z"></path></svg><h1>Page not found</h1>The URL address may be incorrect, or it might have been moved or deleted. Maybe head back to the <a href="/"><strong>Home</strong></a> page?<br>
 </div>
+</div>
 
 <script src="static/darkmode.js"></script>
 
---
+++ baokao/public/error_pages/500.php
@@ -0,0 +1,12 @@
+<?php include "../include/head.php"; ?>
+<?php include "../include/navbar.php"; ?>
+<title>500 Internal Server Error | baokao</title>
+
+<div class="content">
+<div class="flash mt-3 flash-error" style="margin: 20px;" id="no-file" style=""><svg class="octicon octicon-container" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill-rule="evenodd" d="M1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12zm8.036-4.024a.75.75 0 00-1.06 1.06L10.939 12l-2.963 2.963a.75.75 0 101.06 1.06L12 13.06l2.963 2.964a.75.75 0 001.061-1.06L13.061 12l2.963-2.964a.75.75 0 10-1.06-1.06L12 10.939 9.036 7.976z"></path></svg><h1>Internal Server Error</h1>Server had a boo boo. Please contact <a href="mailto:baokao@adrian.id.au">baokao@adrian.id.au</a> and check <a href="https://status.adrian.id.au">status.adrian.id.au</a> for updates.<br>
+</div>
+</div>
+
+<script src="static/darkmode.js"></script>
+
+<?php include "../include/footer.php"; ?>
