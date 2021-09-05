6b925b4c2a7681507cb3c8948084d0d88036ed48
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2021-09-05 16:34:55 +1000

    Copyright info

---
+++ baokao/public/copyright.php
@@ -0,0 +1,32 @@
+<?php include "../include/head.php"; ?>
+<?php include "../include/navbar.php"; ?>
+<title>Copyright | baokao</title>
+
+<div class="content">
+
+    <h1>Copyright</h1>
+    <p class="h3">As an Australian website, we comply with Australian copyright law under <a href="https://www.legislation.gov.au/Series/C1968A00063">Copyright Act 1968 (Cwth)</a>.</p>
+	<hr>
+	<p>
+		<h4>Content we host</h4>
+		<ul class="content">
+			<li>Users retain all rights to information they create and distribute on our site. You may remove your content from baokao at any time through normal means.</li>
+			<li>It is expected that users do not infringe federal copyright law as set out under the Copyright Act 1968 (Cwth.) - doing so may result in a suspension of service to infringing users.</li>
+			<li>We generally permit otherwise-copyrighted material such as past papers, for purposes of research and study under fair dealing. However, users may be asked to remove offending should we receive a valid takedown notice.</li>
+		</ul>
+		<h4>Takedown notices</h4>
+		<ul class="content">
+			<li>If you believe any work of yours has had its rights infringed under the Copyright Act 1968 (Cwth.), please contact <a href="mailto:takedowns@adrian.id.au">takedowns@adrian.id.au</a> with your takedown notice from you or a representative of your organisation. Any claim should clearly state the location of the offending material, proof of ownership/representation, and on what grounds under the Copyright Act the claim is being made under.</li>
+			<li>We generally do not accept claims made under other jurisdictions, including notifications under 17 U.S. Code § 512(c)(3). Any claim submitted needs to be submitted with grounds specified against Copyright Act 1968 (Cwth) - any claims with consideration to legislations of non-Australian jurisdictions, including the United States Digital Millenium Copyright Act, the United States Copyright Act of 1976 or the European Union Directive on Copyright in the Digital Single Market.</li>
+			<li>Any takedown notice issued to baokao under other jurisdictions cannot be received by baokao as we are an Australian service operating under Australian federal law, unless it can be established that your takedown notice is applicable to other jurisdictions.</li>
+			<li>We attempt to consider every takedown notice in good faith and will endeavour to contact you within 7 business days upon receipt of an acceptable takedown notice. Takedown notices which do not comply with the requirements set out in the Copyright Act 1968 (Cwth) may simply be ignored.</li></p>
+		</ul>
+		<div class="flash flash-warn">
+		<p>The Copyright Act allows certain uses of content from the internet without the copyright owner’s permission for purpose of research and study. See Copyright Act No. 63, 1968 Part III Division 3 §40 and Copyright Act No. 63, 1968 Part IV Division 6 §103C.</p>
+	</div>
+	<br>	
+</div>
+
+<script src="static/darkmode.js"></script>
+
+<?php include "../include/footer.php"; ?>
