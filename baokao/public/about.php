0254fdb8900c72a36fa140a634013de6ed45c8d7
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2021-09-05 16:49:50 +1000

    Add copyright to about

--- baokao/public/about.php
+++ baokao/public/about.php
@@ -14,6 +14,7 @@
         <p class="f3-mktg text-bold">a project by <a href="https://adrian.id.au" class="about-link">aidswidjaja</a></p>
         <p class="f3-mktg">credits & changelogs on our <a href="https://github.com/aidswidjaja/baokao" class="about-link">github repository</a></p>
         <p class="f3-mktg">questions? <a href="mailto:baokao@adrian.id.au" class="about-link">baokao@adrian.id.au</a></p>
+        <p class="f6-mktg"><a href="copyright" class="about-link">copyright information</a></p>
     </div>
 </div>
 
